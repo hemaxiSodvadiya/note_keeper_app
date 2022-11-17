03d7b072d272293a5e50ed3f5bf182db3595c9a8
hemaxiSodvadiya <111499619+hemaxiSodvadiya@users.noreply.github.com>
2022-11-17 20:44:20 +0545

    Update home_Page.dart

--- lib/screens/home_Page.dart
+++ lib/screens/home_Page.dart
@@ -1,96 +1,109 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:note_keeper_app/common/note_Card.dart';
+import 'package:note_keeper_app/common/color.dart';
 
-import '../common/add_note.dart';
-import '../common/color.dart';
-import '../common/edit_note.dart';
+import 'border.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class EditNote extends StatefulWidget {
+  const EditNote({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<EditNote> createState() => _EditNoteState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _EditNoteState extends State<EditNote> {
+  int color_id = Random().nextInt(Style.cardsColor.length);
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController contextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Style.mainColor,
+      backgroundColor: Colors.green.shade200,
       appBar: AppBar(
+        backgroundColor: Colors.green.shade200,
+        iconTheme: IconThemeData(color: Colors.black),
         title: Text(
-          "Note Keeper",
-          style: GoogleFonts.roboto(
-              color: Color(0xffccebc0),
-              fontWeight: FontWeight.bold,
-              fontSize: 26),
+          "Add a new Note",
+          style: TextStyle(
+              color: Colors.black, fontSize: 24, fontWeight: FontWeight.w900),
         ),
         centerTitle: true,
-        backgroundColor: Style.mainColor,
       ),
       body: Padding(
-        padding: EdgeInsets.all(14),
+        padding: EdgeInsets.all(10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "Your recent Notes",
-              style: GoogleFonts.roboto(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22),
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                hintText: 'Enter Note Title',
+                labelText: "Enter Note Title",
+                labelStyle: const TextStyle(fontSize: 16, color: Colors.black),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                    width: 3,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.green,
+                    width: 3,
+                  ),
+                ),
+              ),
+              style: Style.mainTitle,
             ),
             SizedBox(
-              height: 20,
+              height: 15,
             ),
-            Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream:
-                    FirebaseFirestore.instance.collection("notes").snapshots(),
-                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  if (snapshot.hasData) {
-                    return GridView(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2),
-                      children: snapshot.data!.docs
-                          .map((note) => noteCard(() {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => AddNote(note),
-                                  ),
-                                );
-                              }, note))
-                          .toList(),
-                    );
-                  }
-                  return Text(
-                    "There is no Notes",
-                    style: GoogleFonts.nunito(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  );
-                },
+            TextField(
+              controller: contextController,
+              keyboardType: TextInputType.multiline,
+              maxLines: 5,
+              decoration: InputDecoration(
+                hintText: 'Enter Note Context',
+                labelText: "Enter Note Context",
+                labelStyle: const TextStyle(fontSize: 16, color: Colors.black),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                    width: 3,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.green,
+                    width: 3,
+                  ),
+                ),
               ),
+              style: Style.mainContext,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
+      floatingActionButton: FloatingActionButton(
         backgroundColor: Style.accentColor,
         onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => EditNote()));
+          FirebaseFirestore.instance.collection("notes").add({
+            "note_title": titleController.text,
+            "note_context": contextController.text,
+            "color_id": color_id,
+          }).then((value) {
+            print(value.id);
+            Navigator.of(context).pop();
+          }).catchError((error) => print("Failed to add new $error"));
         },
-        label: Text("Add Note"),
-        icon: Icon(Icons.add),
+        child: Icon(Icons.save_rounded),
       ),
     );
   }
